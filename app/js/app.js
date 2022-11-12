38022c1b2944ec601bb203156aa8b8936cbfcd27
neverzoom <neverzoom22@gmail.com>
2022-11-12 15:38:23 +0300

    adaptive + functional

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,7 @@
+import $, { nodeName } from 'jquery'
+window.jQuery = $
+window.$ = $
+
 import 'slick-carousel/slick/slick.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,6 +25,37 @@ document.addEventListener('DOMContentLoaded', () => {
 		arrows: false,
 		// autoplay: true,
 		dots: true,
+	});
+
+$('.burger').on('click', () => {
+	if ($('.menu').hasClass('.active') == false) {
+		$('.menu').addClass('active');
+	}
+});
+
+$('.menu .close').on('click', () => {
+	$('.menu').removeClass('active');
+});
+
+$('.modal .close').on('click', () => {
+	$('.modal').removeClass('active');
+});
+
+	//E-mail Ajax Send
+	$("form.recall").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "/mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$('.modal').addClass('active');
+			setTimeout(function() {
+				$('.modal').removeClass('active');
+				th.trigger("reset");
+			}, 5000);
+		});
+		return false;
 	});
 
 })
